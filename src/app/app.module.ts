af12cb3024e5cdbeb63abcd150977b36720e2743
salahuddinquazi <59020425+salahuddinquazi@users.noreply.github.com>
2019-12-27 12:05:07 +0530

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerRegisterationComponent } from './customer-registeration/customer-registeration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerRegisterationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/customer-registeration/customer-registeration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-registeration',
+  templateUrl: './customer-registeration.component.html',
+  styleUrls: ['./customer-registeration.component.sass']
+})
+export class CustomerRegisterationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
